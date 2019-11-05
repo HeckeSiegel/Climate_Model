639768e4f697e4846f05341bbbff00e3971fbdd6
StefanieLegler <stefanielegler@web.de>
2019-11-05 09:32:33 +0100

    new

--- time_loop.c
+++ time_loop.c
@@ -7,11 +7,7 @@
 #define CP 1004.0
 #define g 9.81
 #define Eearth 235. // W/m^2
-#define sigma 5.67e-8
-#define pi 3.14
-#define h 6.626e-34 //Js
-#define c 2.99e8 // m/s
-#define kB 1.38e-23 // m^2kgs^-2K^-1
+#define sigma 5.670374e-8
 
 double T2theta(double T, double p){
     double kappa = RA/CP;
@@ -26,6 +22,9 @@ double E2T(double E, double p, double t){
 }
 double dp2dz(double dp, double p, double T){
     return dp*T*RA/(g*p);
+}
+double boltzmann(double T){
+    return sigma*pow(T,4);
 }
 void convection(double *theta, int size){
     while(1==1){
@@ -52,11 +51,14 @@ int main()
     double p0 = 1000.0; //hPa
     int nlev = 11;
     int nlyr = nlev - 1;
-    int years = 1;
+    int years = 2;
     int nwvl = 3;
 
-    double p[nlev],z[nlev],plyr[nlyr],B_layer[nlyr],B_surface,Tsurf,Tnlev[nlev],T[nlyr],theta[nlyr],Eup[nlev],Edown[nlev],tmp_Eup[nlev],tmp_Edown[nlev],deltaE[nlyr],tau[nlyr],tau0[nlyr];
-    
+    double p[nlev],plyr[nlyr],z[nlev];
+    double B_layer[nlyr],B_surface;
+    double Tsurf,Tnlev[nlev],T[nlyr],theta[nlyr];
+    double Eup[nlev],Edown[nlev],tmp_Eup[nlev],tmp_Edown[nlev],deltaE[nlyr];
+
     //wavelength band
     double wvlband[3][2] = {{1e-6,8e-6},{8e-6,12e-6},{12e-6,1e-4}};
     //pressure profile
@@ -64,16 +66,16 @@ int main()
         p[i] = p0 * (double) i / (double) nlyr;
     }
     //tau profile
-    int window = 1;
-    if(window){
-	for(int inlyr=0; inlyr<nlyr; inlyr++){
-	    tau0[inlyr] = 0.;
-        }
-    }
-    double tau_total = 0.;
+    double tau[nwvl][nlyr], tmp_tau[nlyr];
+    double tau_total = 0.5;
     double dtau = tau_total/nlyr;
     for (int inlyr = 0; inlyr< nlyr; inlyr++){
-        tau[inlyr] = dtau;
+	for (int inwvl = 0; inwvl<nwvl; inwvl++){
+        tau[inwvl][inlyr] = dtau;
+	}
+    }
+    for (int inlyr = 0; inlyr< nlyr; inlyr++){
+	tau[1][inlyr] = 0.;
     }
     //random level temperature profile
     for (int i=0; i < nlev; i++) {
@@ -88,47 +90,38 @@ int main()
     //time loop
     while(t<years*365*24*3600){
         t+=dt;
-        //double dz = dp2dz(100,plyr[nlyr-2],T[nlyr-2])-dp2dz(100,plyr[nlyr-1],T[nlyr-1]);
-	Tsurf = T[nlyr-1];//T[nlyr-1]-dz/2.*1e-2;
-	//heating
-        T[nlyr-1] += E2T(Eearth,plyr[nlyr-1],dt); //heating
+        //surface Temperature for Eup(SURF)
+	Tsurf = T[nlyr-1];
+        //heating
+        T[nlyr-1] = Tsurf + E2T(Eearth/3.,plyr[nlyr-1],dt);
         for(int i = 0; i<nlyr ; i++){
             theta[i] = T2theta(T[i], plyr[i]);
 	}
+        /* gray atmosphere
+        B_surface = B_gray(Tsurf);
+        for(int inlyr=0; inlyr<nlyr; inlyr++){
+	    B_layer[inlyr] = B_gray(T[inlyr]);
+	}
+	schwarzschild(nlev,tau,B_layer,B_surface,Edown,Eup);
+        */
 
-        // nwvl=1 means gray atmosphere
-        if(nwvl == 1){
-	    //Planck profile
-            B_surface = B_gray(Tsurf);
-            for(int inlyr=0; inlyr<nlyr; inlyr++){
-	        B_layer[inlyr] = B_gray(T[inlyr]);
+        //window atmosphere
+	for (int inlev=0; inlev<nlev; inlev++){
+	    Eup[inlev] = 0.;
+	    Edown[inlev] = 0.;
+	}
+	for (int iwvl=0; iwvl<nwvl; iwvl++){
+            B_surface = B_int(wvlband[iwvl][0],wvlband[iwvl][1],Tsurf);
+            for (int inlyr = 0; inlyr< nlyr; inlyr++){
+                B_layer[inlyr] = B_int(wvlband[iwvl][0],wvlband[iwvl][1],T[inlyr]);
+		tmp_tau[inlyr] = tau[iwvl][inlyr];
+            }
+    	    schwarzschild(nlev,tmp_tau,B_layer,B_surface,tmp_Edown,tmp_Eup);
+	    for(int inlev=0; inlev<nlev; inlev++){
+	        Eup[inlev] += tmp_Eup[inlev];
+	        Edown[inlev] += tmp_Edown[inlev];
 	    }
-	    schwarzschild(nlev,tau,B_layer,B_surface,Edown,Eup);
-        }
-
-        //nwvl>1 means radiation transport for each wavelength band separately
-        else{
-	    for (int inlev=0; inlev<nlev; inlev++){
-		Eup[inlev] = 0.;
-		Edown[inlev] = 0.;
-	    }
-	    for (int iwvl=0; iwvl<nwvl; iwvl++){
-                B_surface = B_int(wvlband[iwvl][0],wvlband[iwvl][1],Tsurf);
-                for (int inlyr = 0; inlyr< nlyr; inlyr++){
-                    B_layer[inlyr] = B_int(wvlband[iwvl][0],wvlband[iwvl][1],T[inlyr]);
-                }
-		if(window && iwvl==1){ 
-		    schwarzschild(nlev,tau0,B_layer,B_surface,tmp_Edown,tmp_Eup);
-		}
-		else{
-    	            schwarzschild(nlev,tau,B_layer,B_surface,tmp_Edown,tmp_Eup);
-		}
-	        for(int inlev=0; inlev<nlev; inlev++){
-	            Eup[inlev] += tmp_Eup[inlev];
-	            Edown[inlev] += tmp_Edown[inlev];
-	        }
-	    }
-        }
+	}
     	dE(deltaE,Edown,Eup,nlyr);
     	//deltaT
     	for (int i = 0; i < nlyr; i++){
